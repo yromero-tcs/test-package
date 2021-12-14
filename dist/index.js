c239132eb51c08b1c2436928bfa3f4da30b7f875
Yael Romero <yromero@truechoicesolutions.com>
2021-12-14 17:46:33 -0500

    feat: initial commit of package

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = void 0;
+var helloWorld = function () {
+    return 'Bonjour!';
+};
+exports.helloWorld = helloWorld;
+//# sourceMappingURL=index.js.map
